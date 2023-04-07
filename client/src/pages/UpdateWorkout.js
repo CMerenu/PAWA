38249ce5dc9413ae3e05cb0225e74ad4af301f00
Chikodi Merenu <cmerenu@gmail.com>
2023-04-07 18:04:39 -0500

    upate

--- client/src/pages/UpdateWorkout.js
+++ client/src/pages/UpdateWorkout.js
@@ -65,7 +65,7 @@ const UpdateWorkout = ({ setPlans, getAllPlans, user, userInfo, plans }) => {
 
   if (workoutDetails)
     return (
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <div className="space-y-12">
           <div className="border-b border-white-900/10 pb-12 grid w-screen place-items-center">
             <h2 className="text-base font-semibold leading-7 text-purple-900">
@@ -179,7 +179,6 @@ const UpdateWorkout = ({ setPlans, getAllPlans, user, userInfo, plans }) => {
                 <div className="mt-6 flex items-center justify-end gap-x-6">
                   <button
                     type="submit"
-                    onSubmit={() => handleSubmit}
                     className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                     Upload
