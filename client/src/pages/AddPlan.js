33ef2fe7df6a64ed2d581a063bc1b6fea400a2d8
Chikodi Merenu <cmerenu@gmail.com>
2023-04-08 01:56:46 -0500

    update

--- client/src/pages/AddPlan.js
+++ client/src/pages/AddPlan.js
@@ -130,7 +130,7 @@ const AddPlan = ({ user, userInfo }) => {
                     // onSubmit={() => handleSubmit}
                     className="rounded-md bg-purple-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    Save
+                    Upload
                   </button>
                 </div>
               </div>
